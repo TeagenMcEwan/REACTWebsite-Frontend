64df21ee91aeb0fe0d67c8b9e4698d03fc685afc
TeagenMcEwan <67173830+TeagenMcEwan@users.noreply.github.com>
2020-10-09 23:56:37 +0800

    About page is now displaying

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -16,36 +16,28 @@ function AboutPage() {
           sport, however there is a lack of funding opportunities available as
           sports is a very niche area and there is only a certain amount of
           places up for selection for elite athletes to represent their state,
-          country or on an international platform.
-          </p>
-          <p>
-            Many elite athletes train over 20-35 hours per week to be the
-            champion’s in their fields and ambassadors for WA/Australia, so
-            trying to obtain and retain employment to financially aid the number
-            of state, national and international events and competitions
-            required to meet minimum requirements for sate, national and
-            international selection is extremely difficult as they have to work
-            around their sport training and competition commitments.
-          </p>
-          <p>
-            Every year more and more pressure is put on organisations to provide
-            sponsorship or financial aid to elite athletes to assist them in
-            being able to represent WA and/or Australia. waSportsStars will help
-            to alleviate the pressure of individual requests to organisations
-            each year, and will allow the community, individuals and
-            organisations to fund the individual athlete or team that best suits
-            and aligns to their KPIs and values.
-          </p>
-          <p>
-            waSportsStars will also allow WA elite athletes the opportunity to
-            showcase their talents and selections for financial aid to assist
-            them in representing WA or Australia on state, national or
-            international platforms. The funds pledged to each project will have
-            strict requirements on where the individual athlete or team can
-            spend the funds to ensure they do not miss out on these ‘once in a
-            lifetime’ opportunities. For example travel, accommodation,
-            competition/entry fees and uniforms.
-          </p>
+          country or on an international platform. Many elite athletes train
+          over 20-35 hours per week to be the champion’s in their fields and
+          ambassadors for WA/Australia, so trying to obtain and retain
+          employment to financially aid the number of state, national and
+          international events and competitions required to meet minimum
+          requirements for sate, national and international selection is
+          extremely difficult as they have to work around their sport training
+          and competition commitments. Every year more and more pressure is put
+          on organisations to provide sponsorship or financial aid to elite
+          athletes to assist them in being able to represent WA and/or
+          Australia. waSportsStars will help to alleviate the pressure of
+          individual requests to organisations each year, and will allow the
+          community, individuals and organisations to fund the individual
+          athlete or team that best suits and aligns to their KPIs and values.
+          waSportsStars will also allow WA elite athletes the opportunity to
+          showcase their talents and selections for financial aid to assist them
+          in representing WA or Australia on state, national or international
+          platforms. The funds pledged to each project will have strict
+          requirements on where the individual athlete or team can spend the
+          funds to ensure they do not miss out on these ‘once in a lifetime’
+          opportunities. For example travel, accommodation, competition/entry
+          fees and uniforms.
         </p>
       </div>
     </>
